aa9af46c44ebde10a6c129997f00e1ee0df7af6c
KenDev2105 <195981080+KenDev2105@users.noreply.github.com>
2026-02-14 19:58:47 -0600

    S4C2: Parametros

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,24 @@
-// class Usuario {
-//     public nombre: string;
+class Inventario {
+    id: number;
+    producto: string;
+    codigo: string;
+    fecha: string;
+    precio: number;
 
-//     constructor(nombre: string){
-//         this.nombre = nombre;
-//     }
-// }
+    constructor(id: number, producto: string, codigo: string, fecha: string, precio: number) {
+        this.id = id;
+        this.codigo = codigo;
+        this.producto = producto;
+        this.fecha = fecha;
+        this.precio = precio;
+    }
 
-// const u = new Usuario("Juan");
-// console.log(u.nombre);
+    Agregar(precio2: number, precio3: number): void {
+        console.log(`Producto agregado con precio: $${this.precio}`);
+        console.log(`Precios de descuento: $${precio2} $${precio3}`);
 
-
-// class Usuario {
-//     private password: string;
-//     constructor(pass: string) {
-//         this.password = pass;
-//     }
-
-//     validar(pass: string): boolean {
-//         return this.password === pass;
-//     }
-// }
-
-// const u = new Usuario('1234');
-
-// console.log(u.validar('1234'));
-
-class Persona {
-    constructor(protected edad: number) {
-        this.edad = edad
     }
 }
 
-class Estudiante extends Persona {
-    mostrarEdad() {
-        console.log(this.edad);
-    }
-}
-
-const e = new Estudiante(20);
-
-e.mostrarEdad();
-
-
-class Usuario {
-    public username: string;
-    private password: string;
-
-    constructor(username: string, password: string) {
-        this.username = username;
-        this.password = password;
-    }
-
-    login(pass: string): string {
-        if (pass === this.password) {
-            return "Acceso concedido";
-        } else {
-            return "Contraseña incorrecta";
-        }
-    }
-}
-
-const u = new Usuario('Kenneth', 'kenmej2105')
-console.log(u.login('kenmej2105'));
-
-class Producto {
-    constructor(
-        public nombre: string,
-        private precio: number
-    ) { }
-
-    evaluarPrecio(): string {
-        if (this.precio < 10) {
-            return "Producto barato";
-        } else if (this.precio <= 50) {
-            return "Precio normal";
-        } else {
-            return "Producto caro";
-        }
-    }
-}
-
-const p = new Producto('Chocolate', 2);
-console.log(p.evaluarPrecio());
-
-class Empleado {
-    constructor(
-        public nombre: string,
-        protected salario: number
-    ) { }
-}
-
-class Gerente extends Empleado {
-    bono(): number {
-        if (this.salario > 1000) {
-            return this.salario * 0.2;
-        } else {
-            return this.salario * 0.1;
-        }
-    }
-}
+let producto = new Inventario(1, 'Toallas humedas', 'Psdm32', '2026/02/12', 2.5);
+producto.Agregar(2.45, 2.40);
