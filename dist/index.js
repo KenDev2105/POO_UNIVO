1d11b0f366dad51183fa48e528e869defd16e91f
KenDev2105 <195981080+KenDev2105@users.noreply.github.com>
2026-02-24 09:16:15 -0600

    Semana 5 Clase 2

--- dist/index.js
+++ dist/index.js
@@ -1,73 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//  *** Ejercicio 1 ***
-class informacion {
-    constructor(nombre, apellido, telefono, correo, codIdentificacion) {
+class Persona {
+    constructor(nombre, edad, telefono) {
         this.nombre = nombre;
-        this.apellido = apellido;
+        this.edad = edad;
         this.telefono = telefono;
-        this.correo = correo;
-        this.codIdentificación = codIdentificacion;
     }
     ;
-}
-const info = new informacion("Kenneth", "Mejia", 74950536, "kj_mejia@hotmail.com", "u20250017");
-// console.log(info)
-// *** Ejercicio 2 ***
-class producto {
-    constructor(nombre, stock, precio) {
-        this.nombre = nombre;
-        this.stock = stock;
-        this.precio = precio;
-    }
-    ;
-    descuento(desc) {
-        const descuento = this.precio * desc;
-        const descuento_total = this.precio - descuento;
-        const iva = descuento_total * 0.13;
-        const total = descuento_total + iva;
-        console.log("--- Cantidad de producto: " + this.stock + " ---");
-        console.log("--- Precio: $" + this.precio.toFixed(2) + " ---");
-        console.log("--- Porcentaje de Descuento: " + desc * 100 + "% Descuento aplicado: $" + descuento_total.toFixed(2) + " ---");
-        console.log(" --- IVA: $" + iva.toFixed(2) + " ---");
-        console.log("--- Total a pagar: " + total.toFixed(2) + " ---");
+    mostrar() {
+        alert("El nombre es: " + this.nombre);
     }
 }
-const prod = new producto("Laptop", 10, 1000);
-// prod.descuento(0.75)
-// *** Ejercicio 3 ***
-class edad {
-    constructor(nombre, fecha) {
-        this.nombre = nombre;
-        this.fecha = fecha;
-    }
-    calcularEdad() {
-        let año = this.fecha.slice(6);
-        let edad = 2026 - parseFloat(año);
-        console.log("Su edad es: " + edad);
-        if (edad >= 0 && edad <= 2) {
-            console.log("Es un bebé");
-        }
-        else if (edad > 2 && edad <= 10) {
-            console.log("Es niño/niña");
-        }
-        else if (edad > 10 && edad <= 14) {
-            console.log("Es pre-adolescente");
-        }
-        else if (edad > 14 && edad <= 17) {
-            console.log("Es adolescente");
-        }
-        else if (edad >= 18 && edad < 30) {
-            console.log("Es joven");
-        }
-        else if (edad >= 30 && edad < 50) {
-            console.log("Es adulto");
-        }
-        else if (edad >= 50) {
-            console.log("Es adulto mayor");
-        }
-    }
-}
-const ed = new edad("Ken", "21-05-2006");
-ed.calcularEdad();
+let nombre = prompt("Ingrese su nombre") || "";
+let people = new Persona(nombre, 20, 78789898);
+people.mostrar();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,84 +1,19 @@
-//  *** Ejercicio 1 ***
-class informacion {
-    public nombre: string;
-    public apellido: string;
-    public telefono: number;
-    public correo: string;
-    private codIdentificación: string;
-
-    constructor(nombre:string, apellido: string, telefono: number, correo: string, codIdentificacion:string){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.telefono = telefono;
-        this.correo = correo;
-        this.codIdentificación = codIdentificacion
-    };
-}
-
-const info = new informacion("Kenneth", "Mejia", 74950536, "kj_mejia@hotmail.com", "u20250017")
-// console.log(info)
-
-// *** Ejercicio 2 ***
-
-class producto {
+class Persona {
     nombre: string;
-    stock: number;
-    precio: number;
+    edad: number;
+    telefono: number;
 
-    constructor(nombre: string, stock: number, precio: number) {
-        this.nombre = nombre;
-        this.stock = stock;
-        this.precio = precio
+
+    constructor(nombre: string, edad: number, telefono: number){
+        this.nombre=nombre;
+        this.edad=edad;
+        this.telefono=telefono;
     };
 
-    descuento(desc:number) {
-        const descuento = this.precio * desc
-        const descuento_total = this.precio - descuento
-        const iva = descuento_total * 0.13
-        const total = descuento_total + iva
-
-        console.log("--- Cantidad de producto: " + this.stock + " ---")
-        console.log("--- Precio: $" + this.precio.toFixed(2) + " ---")
-        console.log("--- Porcentaje de Descuento: " + desc*100 + "% Descuento aplicado: $" + descuento_total.toFixed(2) + " ---")
-        console.log(" --- IVA: $" + iva.toFixed(2) + " ---")
-        console.log("--- Total a pagar: " + total.toFixed(2) + " ---")
+    mostrar(): void{
+        alert("El nombre es: " + this.nombre)
     }
 }
-
-const prod = new producto("Laptop", 10, 1000)
-// prod.descuento(0.75)
-
-// *** Ejercicio 3 ***
-class edad {
-    nombre: string;
-    fecha: string;
-
-    constructor(nombre: string, fecha: string) {
-        this.nombre = nombre;
-        this.fecha = fecha;
-    }
-
-    calcularEdad() {
-        let año = this.fecha.slice(6)
-        let edad = 2026 - parseFloat(año)
-        console.log("Su edad es: " + edad)
-        if(edad >= 0 && edad <= 2){
-            console.log("Es un bebé")
-        } else if(edad > 2 && edad <= 10) {
-            console.log("Es niño/niña")
-        } else if(edad > 10 && edad <= 14) {
-            console.log("Es pre-adolescente")
-        } else if(edad > 14 && edad <= 17) {
-            console.log("Es adolescente")
-        } else if(edad >= 18 && edad < 30) {
-            console.log("Es joven")
-        } else if(edad >= 30 && edad < 50) {
-            console.log("Es adulto")
-        } else if(edad >= 50){
-            console.log("Es adulto mayor")
-        }
-    }
-}
-
-const ed = new edad("Ken", "21-05-2006")
-ed.calcularEdad()
+let nombre: string = prompt("Ingrese su nombre") || "";
+let people = new Persona(nombre, 20, 78789898)
+people.mostrar()
