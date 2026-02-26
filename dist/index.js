2d33ff7687d4b6e373136984cea13ff4cd060372
KenDev2105 <195981080+KenDev2105@users.noreply.github.com>
2026-02-26 09:30:41 -0600

    P1 Examples

--- dist/index.js
+++ dist/index.js
@@ -1,140 +1,128 @@
-import console from "console";
-import { read } from "fs";
-import * as readline from "readline";
-import { runInThisContext } from "vm";
-// ! Ejemplo de Clase
-/* class Programa {
-    private rl: readline.Interface;
-
-    constructor(){
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
-    }
-    iniciar(): void{
-        this.pedirNumero();
-    }
-    pedirNumero(): void{
-        this.rl.question("Ingresa un número: ", (respuesta:string) =>{
-            const numero = Number(respuesta)
-            this.evaluarNumero(numero);
-            this.cerrar()
-        })
-    }
-
-    evaluarParImpar(num:number): void{
-        if(num % 2 === 0){
-            console.log("Numero par")
-        } else{
-            console.log("Numero impar")
-        }
-    }
-
-    evaluarNumero(numero: number): void{
-        if(numero > 10){
-            console.log("Es mayor que 10")
-        } else if (numero === 10){
-            console.log("Es igual a 10")
-        } else {
-            console.log("Es menor que 10")
-        }
-    }
-
-    cerrar(): void{
-        this.rl.close()
-    }
-}
-
-const app = new Programa()
-app.iniciar() */
-//? Ejercicio 1
-/* class Escuela {
-    private rl: readline.Interface;
-
-    constructor(){
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-    }
-
-    iniciarEscuela(): void{
-        this.pedirNota()
-    }
-
-    pedirNota(): void{
-        this.rl.question("Ingresa la nota: ", (nota:string) => {
-            const numero = parseFloat(nota);
-            this.estadoAcademico(numero);
-            this.cerrar();
-        })
-    }
-
-    estadoAcademico(nota:number):void{
-        if(nota >= 9 && nota <= 10){
-            console.log("Excelente")
-        } else if(nota >= 7 && nota < 9){
-            console.log("Bueno")
-        } else if (nota >= 6 && nota < 7){
-            console.log("Regular")
-        } else if(nota < 6){
-            console.log("Reprobado")
-        }
-    }
-
-    cerrar(): void{
-        this.rl.close();
-    }
-}
-
-const estudiante = new Escuela();
-estudiante.iniciarEscuela();
- */
-// ? Ejercicio 2
-class Autenticacion {
-    constructor() {
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-    }
-    iniciarAutenticacion() {
-        this.pedirDatos();
-    }
-    pedirDatos() {
-        this.rl.question("Ingresa tu usuario: ", (user) => {
-            this.rl.question("Ingresa tu contraseña: ", (pass) => {
-                this.rl.question("Ingrese su rol: 1 = Administrador, 2 = Cliente, 3 = Invitado ", (rol) => {
-                    const numero = Number(rol);
-                    this.autenticarDatos(user, pass, numero);
-                    this.cerrar();
-                });
-            });
-        });
-    }
-    autenticarDatos(user, pass, rol) {
-        if (user === "kendev2105" && pass === "12345678" && rol === 1) {
-            console.log("Acceso concedido");
-            console.log("Usted es Administrador");
-        }
-        else if (user === "kendev2105" && pass === "12345678" && rol === 2) {
-            console.log("Acceso concedido");
-            console.log("Usted es Cliente");
-        }
-        else if (user === "kendev2105" && pass === "12345678" && rol === 3) {
-            console.log("Acceso concedido");
-            console.log("Usted es Invitado");
-        }
-        else {
-            console.log("Acceso denegado, revise usuario o contraseña");
-        }
-    }
-    cerrar() {
-        this.rl.close();
+/* Ejemplos linea 1-135
+   Ejercicios linea 138 en delante
+*/
+// Ejemplo 1
+class Player {
+    constructor(nombre, vida) {
+        this.nombre = nombre;
+        this.vida = vida;
     }
     ;
+    recibirDaño() {
+        const puntos = Number(Math.random().toFixed(2)) * 100;
+        this.vida -= puntos;
+        console.log(`Has recibido ${puntos.toFixed(0)} de daño, tu vida actual es de:  ${this.vida}`);
+    }
+    ;
+    status() {
+        if (this.vida <= 0) {
+            console.log("Has muerto!");
+        }
+        else {
+            console.log(`Sigues con vida! ${this.nombre}, estas lleno de Determinación`);
+        }
+    }
 }
-const autenticar = new Autenticacion();
-autenticar.iniciarAutenticacion();
+/* const pj = new Player("Ken", 100)
+pj.recibirDaño()
+pj.status() */
+// Ejemplo 2
+class OrdenCafe {
+    constructor(tipoCafe, cantidad, precio) {
+        this.tipoCafe = tipoCafe;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+    totalPagar(cantidad) {
+        if (cantidad > this.cantidad) {
+            const total = cantidad * this.precio;
+            console.log(`Su total a pagar es de $${total}`);
+        }
+        else {
+            console.log("No hay cantidad suficiente de producto o stock del mismo");
+        }
+    }
+}
+/* const pedido = new OrdenCafe("Capuccino", 20, 4)
+pedido.totalPagar(21) */
+// Ejemplo 3
+class Mascota {
+    constructor(nombre, hambre) {
+        this.nombre = nombre;
+        this.hambre = hambre;
+    }
+    comer() {
+        this.hambre -= 20;
+        console.log(`El hambre ha disminuido en 20, tienes ${this.hambre} de hambre`);
+    }
+    correr() {
+        this.hambre += 10;
+        console.log(`El hambre ha incrementado en 10, tiene ${this.hambre} de hambre`);
+    }
+}
+/* const perro = new Mascota("Balú",100)
+perro.comer()
+perro.correr() */
+// Ejemplo 4
+class Alarma {
+    constructor(codigo) {
+        this.estado = true;
+        this.codigo = codigo;
+    }
+    desactivar(intento) {
+        if (intento === this.codigo) {
+            this.estado = false;
+            console.log("El codigo de acceso es valido, continue");
+        }
+        else {
+            console.log("EL codigo de acceso es incorrecto");
+        }
+    }
+}
+/* const alarma = new Alarma("u20250017")
+alarma.desactivar("u20250017") */
+// Ejemplo 5
+class TanqueCombustible {
+    constructor(capacidadMaxima, nivelActual) {
+        this.capacidadMaxima = 100;
+        this.nivelActual = 60;
+        this.capacidadMaxima = capacidadMaxima;
+        this.nivelActual = nivelActual;
+    }
+    agregarCombustible(agregar) {
+        if (this.nivelActual <= this.capacidadMaxima) {
+            this.nivelActual += agregar;
+            console.log(`Tienes ${this.nivelActual} de gasolina`);
+        }
+        else {
+            console.log(`Has superado el limite maximo del tanque`);
+        }
+    }
+    consumirCombustible() {
+        if (this.nivelActual === 0) {
+            console.log("No tienes gasolina");
+        }
+        else if (this.nivelActual > 0 && this.nivelActual <= 15) {
+            console.log("Tienes poca gasolina");
+        }
+    }
+}
+/* const corvette = new TanqueCombustible(100,45)
+corvette.agregarCombustible(56) */
+// Ejercicio 1
+class Empleado {
+    constructor(nombre, salarioBase, horasExtra) {
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.horasExtra = horasExtra;
+    }
+    salarioTotal(cantidadHoras) {
+        this.horasExtra *= cantidadHoras;
+        const total = this.salarioBase + this.horasExtra;
+        console.log(`Su salario base es de: $${this.salarioBase.toFixed(2)} \n Su total de horas extra trabajadas es de: ${cantidadHoras} por un monto de $${this.horasExtra.toFixed(2)} \n Su pago total es de: $${total}`);
+    }
+}
+const empleado = new Empleado("Ken", 1000, 100);
+empleado.salarioTotal(2);
+export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,147 +1,179 @@
-import console from "console";
-import { read } from "fs";
-import * as readline from "readline"
-import { runInThisContext } from "vm";
+/* Ejemplos linea 1-135
+   Ejercicios linea 138 en delante
+*/
 
-// ! Ejemplo de Clase
-/* class Programa {
-    private rl: readline.Interface;
+// Ejemplo 1
+class Player{
+    nombre: string;
+    vida: number;
 
-    constructor(){
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        })
-    }
-    iniciar(): void{
-        this.pedirNumero();
-    }
-    pedirNumero(): void{
-        this.rl.question("Ingresa un número: ", (respuesta:string) =>{
-            const numero = Number(respuesta)
-            this.evaluarNumero(numero);
-            this.cerrar()
-        })
-    }
-
-    evaluarParImpar(num:number): void{
-        if(num % 2 === 0){
-            console.log("Numero par")
-        } else{
-            console.log("Numero impar")
-        }
-    }
-
-    evaluarNumero(numero: number): void{
-        if(numero > 10){
-            console.log("Es mayor que 10")
-        } else if (numero === 10){
-            console.log("Es igual a 10")
-        } else {
-            console.log("Es menor que 10")
-        }
-    }
-
-    cerrar(): void{
-        this.rl.close()
-    }
-}
-
-const app = new Programa()
-app.iniciar() */
-
-//? Ejercicio 1
-
-/* class Escuela {
-    private rl: readline.Interface;
-
-    constructor(){
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-    }
-
-    iniciarEscuela(): void{
-        this.pedirNota()
-    }
-
-    pedirNota(): void{
-        this.rl.question("Ingresa la nota: ", (nota:string) => {
-            const numero = parseFloat(nota);
-            this.estadoAcademico(numero);
-            this.cerrar();
-        })
-    }
-
-    estadoAcademico(nota:number):void{
-        if(nota >= 9 && nota <= 10){
-            console.log("Excelente")
-        } else if(nota >= 7 && nota < 9){
-            console.log("Bueno")
-        } else if (nota >= 6 && nota < 7){
-            console.log("Regular")
-        } else if(nota < 6){
-            console.log("Reprobado")
-        }
-    }
-
-    cerrar(): void{
-        this.rl.close();
-    }
-} 
-
-const estudiante = new Escuela();
-estudiante.iniciarEscuela();
- */
-// ? Ejercicio 2
-
-class Autenticacion{
-    private rl: readline.Interface;
-
-    constructor(){
-        this.rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-    }
-
-    iniciarAutenticacion(): void{
-        this.pedirDatos();
-    }
-
-    pedirDatos(): void{
-        this.rl.question("Ingresa tu usuario: ", (user:string) => {
-            this.rl.question("Ingresa tu contraseña: ", (pass:string) =>{
-                this.rl.question("Ingrese su rol: 1 = Administrador, 2 = Cliente, 3 = Invitado ", (rol:string) =>{
-                const numero = Number(rol)
-                this.autenticarDatos(user, pass, numero)
-                this.cerrar()
-            })
-                })
-        })
-    }
-
-    autenticarDatos(user:string, pass:string, rol:Number): void{
-        if(user === "kendev2105" && pass === "12345678" && rol === 1){
-            console.log("Acceso concedido")
-            console.log("Usted es Administrador")
-        } else if(user === "kendev2105" && pass === "12345678" && rol === 2){
-            console.log("Acceso concedido")
-            console.log("Usted es Cliente") 
-        } else if(user === "kendev2105" && pass === "12345678" && rol === 3){
-            console.log("Acceso concedido")
-            console.log("Usted es Invitado")
-        } else{
-            console.log("Acceso denegado, revise usuario o contraseña")
-        }
-    }
-
-
-    cerrar(): void{
-        this.rl.close();
+    constructor(nombre:string, vida:number){
+        this.nombre = nombre;
+        this.vida = vida;
     };
+
+    recibirDaño(): void{
+        const puntos = Number(Math.random().toFixed(2))*100
+        this.vida -= puntos
+        console.log(`Has recibido ${puntos.toFixed(0)} de daño, tu vida actual es de:  ${this.vida}`)
+    };
+
+    status():void {
+        if(this.vida <= 0){
+            console.log("Has muerto!")
+        } else{
+            console.log(`Sigues con vida! ${this.nombre}, estas lleno de Determinación`  )
+        }
+    }
 }
 
-const autenticar = new Autenticacion();
-autenticar.iniciarAutenticacion();
+/* const pj = new Player("Ken", 100)
+pj.recibirDaño()
+pj.status() */
+
+// Ejemplo 2
+class OrdenCafe{
+    tipoCafe:string;
+    cantidad:number;
+    precio:number;
+
+    constructor(tipoCafe:string, cantidad:number, precio: number){
+        this.tipoCafe = tipoCafe;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    totalPagar(cantidad:number){
+        if(cantidad > this.cantidad){
+            const total = cantidad * this.precio
+            console.log(`Su total a pagar es de $${total}`)
+        } else{
+            console.log("No hay cantidad suficiente de producto o stock del mismo")
+        }
+    }
+}
+
+/* const pedido = new OrdenCafe("Capuccino", 20, 4)
+pedido.totalPagar(21) */
+
+// Ejemplo 3
+class Mascota{
+    nombre: string;
+    hambre: number;
+
+    constructor(nombre: string, hambre:number){
+        this.nombre = nombre;
+        this.hambre = hambre;
+    }
+
+    comer(){
+        this.hambre -= 20
+        console.log(`El hambre ha disminuido en 20, tienes ${this.hambre} de hambre`)
+    }
+
+    correr(){
+        this.hambre += 10
+        console.log(`El hambre ha incrementado en 10, tiene ${this.hambre} de hambre`)
+    }
+}
+
+/* const perro = new Mascota("Balú",100)
+perro.comer()
+perro.correr() */
+
+// Ejemplo 4
+class Alarma{
+    codigo: string;
+    estado: boolean = true;
+
+    constructor(codigo:string){
+        this.codigo = codigo;
+    }
+
+    desactivar(intento: string):void{
+        if(intento === this.codigo){
+            this.estado = false
+            console.log("El codigo de acceso es valido, continue")
+        } else{
+            console.log("EL codigo de acceso es incorrecto")
+        }
+    }
+}
+
+/* const alarma = new Alarma("u20250017")
+alarma.desactivar("u20250017") */
+
+// Ejemplo 5
+
+class TanqueCombustible{
+    capacidadMaxima: number = 100;
+    nivelActual: number = 60;
+
+    constructor(capacidadMaxima:number, nivelActual:number){
+        this.capacidadMaxima = capacidadMaxima;
+        this.nivelActual = nivelActual
+    }
+
+    agregarCombustible(agregar:number):void{
+        if(this.nivelActual <= this.capacidadMaxima){
+            this.nivelActual += agregar
+            console.log(`Tienes ${this.nivelActual} de gasolina`)
+        } else{
+            console.log(`Has superado el limite maximo del tanque`)
+        }
+    }
+
+    consumirCombustible():void{
+        if(this.nivelActual === 0){
+            console.log("No tienes gasolina")
+        } else if(this.nivelActual > 0 && this.nivelActual <= 15){
+            console.log("Tienes poca gasolina")
+        }
+    }
+}
+
+/* const corvette = new TanqueCombustible(100,45)
+corvette.agregarCombustible(56) */
+
+// Ejercicio 1
+class Empleado{
+    nombre: string;
+    salarioBase: number;
+    horasExtra: number;
+
+    constructor(nombre:string, salarioBase: number, horasExtra:number){
+        this.nombre = nombre;
+        this.salarioBase = salarioBase;
+        this.horasExtra = horasExtra;
+    }
+
+    salarioTotal(cantidadHoras:number){
+        this.horasExtra *= cantidadHoras
+        const total = this.salarioBase + this.horasExtra
+        console.log(`Su salario base es de: $${this.salarioBase.toFixed(2)} \n Su total de horas extra trabajadas es de: ${cantidadHoras} por un monto de $${this.horasExtra.toFixed(2)} \n Su pago total es de: $${total}`)
+    }
+}
+
+const empleado = new Empleado("Ken", 1000, 100);
+empleado.salarioTotal(2)
+
+class cuentaBancaria{
+    titular: string;
+    saldo: number = 0;
+    
+    constructor(titular:string){
+        this.titular = titular;
+    }
+
+    depositar():void{
+        
+    }
+
+    retirar():void{
+
+    }
+
+    consultarSaldo(): void{
+
+    }
+}
