014b8a8231fda39805ee644c9aa1b8b0ed3b41a9
KenDev2105 <195981080+KenDev2105@users.noreply.github.com>
2026-02-26 10:05:15 -0600

    S6C2

--- dist/index.js
+++ dist/index.js
@@ -1,128 +1,37 @@
-/* Ejemplos linea 1-135
-   Ejercicios linea 138 en delante
-*/
-// Ejemplo 1
-class Player {
-    constructor(nombre, vida) {
+// ? Ejemplo clase
+class Animales {
+    constructor(nombre, categoria, raza) {
         this.nombre = nombre;
-        this.vida = vida;
-    }
-    ;
-    recibirDaño() {
-        const puntos = Number(Math.random().toFixed(2)) * 100;
-        this.vida -= puntos;
-        console.log(`Has recibido ${puntos.toFixed(0)} de daño, tu vida actual es de:  ${this.vida}`);
-    }
-    ;
-    status() {
-        if (this.vida <= 0) {
-            console.log("Has muerto!");
-        }
-        else {
-            console.log(`Sigues con vida! ${this.nombre}, estas lleno de Determinación`);
-        }
-    }
-}
-/* const pj = new Player("Ken", 100)
-pj.recibirDaño()
-pj.status() */
-// Ejemplo 2
-class OrdenCafe {
-    constructor(tipoCafe, cantidad, precio) {
-        this.tipoCafe = tipoCafe;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-    totalPagar(cantidad) {
-        if (cantidad > this.cantidad) {
-            const total = cantidad * this.precio;
-            console.log(`Su total a pagar es de $${total}`);
-        }
-        else {
-            console.log("No hay cantidad suficiente de producto o stock del mismo");
-        }
-    }
-}
-/* const pedido = new OrdenCafe("Capuccino", 20, 4)
-pedido.totalPagar(21) */
-// Ejemplo 3
-class Mascota {
-    constructor(nombre, hambre) {
-        this.nombre = nombre;
-        this.hambre = hambre;
-    }
-    comer() {
-        this.hambre -= 20;
-        console.log(`El hambre ha disminuido en 20, tienes ${this.hambre} de hambre`);
+        this.categoria = categoria;
+        this.raza = raza;
     }
     correr() {
-        this.hambre += 10;
-        console.log(`El hambre ha incrementado en 10, tiene ${this.hambre} de hambre`);
+        const print = console.log(`${this.nombre} corre 40km/h ya que es un ${this.categoria} de la raza ${this.raza}`);
+        return print;
     }
 }
-/* const perro = new Mascota("Balú",100)
-perro.comer()
-perro.correr() */
-// Ejemplo 4
-class Alarma {
-    constructor(codigo) {
-        this.estado = true;
-        this.codigo = codigo;
+class Encargado extends Animales {
+    constructor(nombre, categoria, raza, dueño) {
+        super(nombre, categoria, raza);
+        this.dueño = dueño;
     }
-    desactivar(intento) {
-        if (intento === this.codigo) {
-            this.estado = false;
-            console.log("El codigo de acceso es valido, continue");
-        }
-        else {
-            console.log("EL codigo de acceso es incorrecto");
-        }
+    mostrar() {
+        return console.log(`${this.dueño} se la pica solo porque su mascota llamada ${this.nombre} es de raza ${this.raza}`);
     }
 }
-/* const alarma = new Alarma("u20250017")
-alarma.desactivar("u20250017") */
-// Ejemplo 5
-class TanqueCombustible {
-    constructor(capacidadMaxima, nivelActual) {
-        this.capacidadMaxima = 100;
-        this.nivelActual = 60;
-        this.capacidadMaxima = capacidadMaxima;
-        this.nivelActual = nivelActual;
+// Ejercicio
+class Venta extends Encargado {
+    constructor(nombre, categoria, raza, dueño, precio) {
+        super(nombre, categoria, raza, dueño);
+        this.precio = precio;
     }
-    agregarCombustible(agregar) {
-        if (this.nivelActual <= this.capacidadMaxima) {
-            this.nivelActual += agregar;
-            console.log(`Tienes ${this.nivelActual} de gasolina`);
-        }
-        else {
-            console.log(`Has superado el limite maximo del tanque`);
-        }
-    }
-    consumirCombustible() {
-        if (this.nivelActual === 0) {
-            console.log("No tienes gasolina");
-        }
-        else if (this.nivelActual > 0 && this.nivelActual <= 15) {
-            console.log("Tienes poca gasolina");
-        }
+    vender() {
+        console.log(`${this.nombre} se vende a $${this.precio}`);
     }
 }
-/* const corvette = new TanqueCombustible(100,45)
-corvette.agregarCombustible(56) */
-// Ejercicio 1
-class Empleado {
-    constructor(nombre, salarioBase, horasExtra) {
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-        this.horasExtra = horasExtra;
-    }
-    salarioTotal(cantidadHoras) {
-        this.horasExtra *= cantidadHoras;
-        const total = this.salarioBase + this.horasExtra;
-        console.log(`Su salario base es de: $${this.salarioBase.toFixed(2)} \n Su total de horas extra trabajadas es de: ${cantidadHoras} por un monto de $${this.horasExtra.toFixed(2)} \n Su pago total es de: $${total}`);
-    }
-}
-const empleado = new Empleado("Ken", 1000, 100);
-empleado.salarioTotal(2);
+const exec = new Venta("Balú", "perro", "Husky", "Ken", 20);
+exec.mostrar();
+exec.correr();
+exec.vender();
 export {};
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,179 +1,48 @@
-/* Ejemplos linea 1-135
-   Ejercicios linea 138 en delante
-*/
+// ? Ejemplo clase
 
-// Ejemplo 1
-class Player{
-    nombre: string;
-    vida: number;
+class Animales{
+    nombre:string;
+    categoria:string;
+    raza:string;
 
-    constructor(nombre:string, vida:number){
+    constructor(nombre:string, categoria:string, raza:string){
         this.nombre = nombre;
-        this.vida = vida;
-    };
-
-    recibirDaño(): void{
-        const puntos = Number(Math.random().toFixed(2))*100
-        this.vida -= puntos
-        console.log(`Has recibido ${puntos.toFixed(0)} de daño, tu vida actual es de:  ${this.vida}`)
-    };
-
-    status():void {
-        if(this.vida <= 0){
-            console.log("Has muerto!")
-        } else{
-            console.log(`Sigues con vida! ${this.nombre}, estas lleno de Determinación`  )
-        }
-    }
-}
-
-/* const pj = new Player("Ken", 100)
-pj.recibirDaño()
-pj.status() */
-
-// Ejemplo 2
-class OrdenCafe{
-    tipoCafe:string;
-    cantidad:number;
-    precio:number;
-
-    constructor(tipoCafe:string, cantidad:number, precio: number){
-        this.tipoCafe = tipoCafe;
-        this.cantidad = cantidad;
-        this.precio = precio;
-    }
-
-    totalPagar(cantidad:number){
-        if(cantidad > this.cantidad){
-            const total = cantidad * this.precio
-            console.log(`Su total a pagar es de $${total}`)
-        } else{
-            console.log("No hay cantidad suficiente de producto o stock del mismo")
-        }
-    }
-}
-
-/* const pedido = new OrdenCafe("Capuccino", 20, 4)
-pedido.totalPagar(21) */
-
-// Ejemplo 3
-class Mascota{
-    nombre: string;
-    hambre: number;
-
-    constructor(nombre: string, hambre:number){
-        this.nombre = nombre;
-        this.hambre = hambre;
-    }
-
-    comer(){
-        this.hambre -= 20
-        console.log(`El hambre ha disminuido en 20, tienes ${this.hambre} de hambre`)
+        this.categoria = categoria;
+        this.raza = raza;
     }
 
     correr(){
-        this.hambre += 10
-        console.log(`El hambre ha incrementado en 10, tiene ${this.hambre} de hambre`)
+        const print = console.log(`${this.nombre} corre 40km/h ya que es un ${this.categoria} de la raza ${this.raza}`)
+        return print
     }
 }
 
-/* const perro = new Mascota("Balú",100)
-perro.comer()
-perro.correr() */
-
-// Ejemplo 4
-class Alarma{
-    codigo: string;
-    estado: boolean = true;
-
-    constructor(codigo:string){
-        this.codigo = codigo;
+class Encargado extends Animales{
+    dueño:string
+    constructor(nombre:string, categoria:string, raza:string, dueño:string){
+        super(nombre,categoria,raza)
+        this.dueño = dueño
     }
 
-    desactivar(intento: string):void{
-        if(intento === this.codigo){
-            this.estado = false
-            console.log("El codigo de acceso es valido, continue")
-        } else{
-            console.log("EL codigo de acceso es incorrecto")
-        }
+    mostrar(){
+        return console.log(`${this.dueño} se la pica solo porque su mascota llamada ${this.nombre} es de raza ${this.raza}`)
     }
 }
 
-/* const alarma = new Alarma("u20250017")
-alarma.desactivar("u20250017") */
-
-// Ejemplo 5
-
-class TanqueCombustible{
-    capacidadMaxima: number = 100;
-    nivelActual: number = 60;
-
-    constructor(capacidadMaxima:number, nivelActual:number){
-        this.capacidadMaxima = capacidadMaxima;
-        this.nivelActual = nivelActual
+// Ejercicio
+class Venta extends Encargado{
+    precio:number;
+    constructor(nombre:string, categoria:string, raza:string, dueño:string, precio:number){
+        super(nombre,categoria,raza, dueño)
+        this.precio = precio
     }
 
-    agregarCombustible(agregar:number):void{
-        if(this.nivelActual <= this.capacidadMaxima){
-            this.nivelActual += agregar
-            console.log(`Tienes ${this.nivelActual} de gasolina`)
-        } else{
-            console.log(`Has superado el limite maximo del tanque`)
-        }
+    vender(){
+        console.log(`${this.nombre} se vende a $${this.precio}`)
     }
+} 
 
-    consumirCombustible():void{
-        if(this.nivelActual === 0){
-            console.log("No tienes gasolina")
-        } else if(this.nivelActual > 0 && this.nivelActual <= 15){
-            console.log("Tienes poca gasolina")
-        }
-    }
-}
-
-/* const corvette = new TanqueCombustible(100,45)
-corvette.agregarCombustible(56) */
-
-// Ejercicio 1
-class Empleado{
-    nombre: string;
-    salarioBase: number;
-    horasExtra: number;
-
-    constructor(nombre:string, salarioBase: number, horasExtra:number){
-        this.nombre = nombre;
-        this.salarioBase = salarioBase;
-        this.horasExtra = horasExtra;
-    }
-
-    salarioTotal(cantidadHoras:number){
-        this.horasExtra *= cantidadHoras
-        const total = this.salarioBase + this.horasExtra
-        console.log(`Su salario base es de: $${this.salarioBase.toFixed(2)} \n Su total de horas extra trabajadas es de: ${cantidadHoras} por un monto de $${this.horasExtra.toFixed(2)} \n Su pago total es de: $${total}`)
-    }
-}
-
-const empleado = new Empleado("Ken", 1000, 100);
-empleado.salarioTotal(2)
-
-class cuentaBancaria{
-    titular: string;
-    saldo: number = 0;
-    
-    constructor(titular:string){
-        this.titular = titular;
-    }
-
-    depositar():void{
-        
-    }
-
-    retirar():void{
-
-    }
-
-    consultarSaldo(): void{
-
-    }
-}
+const exec = new Venta("Balú", "perro", "Husky", "Ken", 20)
+exec.mostrar()
+exec.correr()
+exec.vender()
